8d298d1fabc4794bd9ae582227b414e4d0aceceb
Uriel Arriaga <joarriag@cisco.com>
2020-07-02 13:52:13 -0500

    beta no front

--- app/models.py
+++ app/models.py
@@ -3,10 +3,9 @@ from app import db
 
 
 class btc_mxn(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    last = db.Column(db.String(20), unique=True, nullable=False)
-    volume = db.Column(db.String(120), unique=True, nullable=False)
-    created_at = db.Column(db.String(120), unique=True, nullable=False)
+    last = db.Column(db.String(20),  nullable=False)
+    volume = db.Column(db.String(120),  nullable=False)
+    created_at = db.Column(db.String(20), unique=True, nullable=False, primary_key=True)
 
     def __repr__(self):
         return f"btc_mxn('{self.last}', '{self.volume}', '{self.created_at}')"
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 import requests
 from flask import render_template, url_for, flash, redirect
 from app import app, db
+from datetime import datetime
 from app.models import btc_mxn
 
 
@@ -12,7 +13,11 @@ def home():
     respuesta = requests.get("https://api.bitso.com/v3/ticker/?book=btc_mxn").json()
     if respuesta["success"]:
         datos = respuesta["payload"]
-        registro = btc_mxn(last = datos["last"],volume = datos["volume"] ,created_at = datos["created_at"])
+        print(datos["created_at"])
+        fecha,horario =  datos["created_at"].split("T")
+        ano,mes,dia = fecha.split("-")
+        hora,minuto,segundo = horario.split("+")[0].split(":")
+        registro = btc_mxn(last = datos["last"],volume = datos["volume"] ,created_at = datetime(int(ano),int(mes),int(dia),int(hora),int(minuto)).strftime('%s'))
         db.session.add(registro)
         db.session.commit()
         print(respuesta)
